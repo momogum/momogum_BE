d64538bc326d64abffad3b299efcc0ccb11ac835
gdbs1107 <gdbs1107@naver.com>
2024-12-30 22:01:51 +0900

    [init] 프로젝트 초기설정

---
+++ src/main/java/com/example/momogum/MomogumApplication.java
@@ -0,0 +1,13 @@
+package com.example.momogum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomogumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomogumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/momogum/MomogumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.momogum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomogumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

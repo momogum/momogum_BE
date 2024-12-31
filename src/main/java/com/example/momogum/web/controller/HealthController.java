61b9cd15e704d9685ca20726f37831e613a2d2e2
gdbs1107 <gdbs1107@naver.com>
2024-12-31 16:44:04 +0900

    [chore] 서버 안정성 체크 컨트롤러 태그 추가

--- src/main/java/com/example/momogum/web/controller/HealthController.java
+++ src/main/java/com/example/momogum/web/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.example.momogum.web.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@Tag(name = "서버 안정성 체크 API")
 public class HealthController {
 
     @GetMapping

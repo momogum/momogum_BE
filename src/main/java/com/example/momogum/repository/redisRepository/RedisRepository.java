358a9be22b700e37ffcbbaa2e09f33110cd75c68
Hwigun46 <hy010827@naver.com>
2025-01-22 04:23:58 +0900

    [fix] 주석 수정

--- src/main/java/com/example/momogum/repository/redisRepository/RedisRepository.java
+++ src/main/java/com/example/momogum/repository/redisRepository/RedisRepository.java
@@ -16,13 +16,13 @@ public class RedisRepository {
     private final RedisTemplate<String, Object> redisTemplate;
 
 
-    // 1. 사용자가 조회한 postId 저장
+    // 사용자가 조회한 postId 저장
     public void addViewedPost(String userId, String postId) {
         String key = generateKey(userId);
         redisTemplate.opsForSet().add(key, postId);
     }
 
-    // 2. 사용자가 조회한 postId 목록 조회
+    // 사용자가 조회한 postId 목록 조회
     public Set<Long> getViewedPosts(String userId) {
         String key = generateKey(userId);
 
@@ -32,12 +32,12 @@ public class RedisRepository {
                 .collect(Collectors.toSet());
     }
 
-    // 3. Redis Key 생성
+    // Redis Key 생성
     private String generateKey(String userId) {
         return "user:" + userId + ":viewed";
     }
 
-    // 4. 세션 만료 시간 설정
+    //  세션 만료 시간 설정
     public void setSessionTimeout(String userId, long timeoutMinutes) {
         String key = generateKey(userId);
         redisTemplate.expire(key, Duration.ofMinutes(timeoutMinutes));

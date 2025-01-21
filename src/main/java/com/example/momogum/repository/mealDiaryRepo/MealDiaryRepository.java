1c0bc87c38113ea20f92a78a3b94c747311c01d0
Hwigun46 <hy010827@naver.com>
2025-01-21 18:17:12 +0900

    [feat] MealDiary 변동 사항 수정

--- src/main/java/com/example/momogum/repository/mealDiaryRepo/MealDiaryRepository.java
+++ src/main/java/com/example/momogum/repository/mealDiaryRepo/MealDiaryRepository.java
@@ -2,11 +2,21 @@ package com.example.momogum.repository.mealDiaryRepo;
 
 import com.example.momogum.domain.MealDiary;
 import com.example.momogum.domain.UserEntity;
+import com.example.momogum.web.dto.viewMealDiary.ViewMealDiaryDTO;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 import java.util.List;
 
 public interface MealDiaryRepository extends JpaRepository<MealDiary,Long> {
 
     List<MealDiary> findByUserEntity(UserEntity userEntity);
+    @Query("SELECT m.id FROM MealDiary m WHERE m.isRevisit = :isRevisit ORDER BY m.likesCount DESC")
+    List<Long> findAllByIsRevisit(@Param("isRevisit") String isRevisit);
+
+    List<MealDiary> findByIdIn(List<Long> ids);
+
 }

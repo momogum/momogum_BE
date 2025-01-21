cfe3a659946994a46e09b809b2fdd0f0763a44f1
Hwigun46 <hy010827@naver.com>
2025-01-22 04:26:43 +0900

    [feat] 각 로직 별 api 추가

--- src/main/java/com/example/momogum/web/controller/viewMealPlan/ViewMealPlanController.java
+++ src/main/java/com/example/momogum/web/controller/viewMealPlan/ViewMealPlanController.java
@@ -17,11 +17,26 @@ public class ViewMealPlanController {
     private final ViewMealDiaryService viewMealDiaryService;
 
     @Operation(summary = "메인 페이지 또 올래요 시 조회",
-            description = "메인 페이지에서 또 올래요를 눌렀을 때 ")
+            description = "메인 페이지에서 또 올래요를 눌렀을 때 사용될 API입니다. ")
     @GetMapping("/revisit")
-    public ApiResponse<ViewMealDiaryDTO.ViewMealDiaryResponseListDTO> getMealDiaryIsRevisit(@RequestParam Long userId) {
+    public ApiResponse<ViewMealDiaryDTO.ViewMealDiaryResponseListDTO> getMealDiaryIsRevisit(
+            @RequestParam Long userId) {
 
         ViewMealDiaryDTO.ViewMealDiaryResponseListDTO response = viewMealDiaryService.getMealDiaryIsRevisitedByLikesCount(userId);
+
+        return ApiResponse.onSuccess(response);
+
+    }
+
+    @Operation(summary = "메인 페이지 음식 카테고리 조회",
+            description = "메인 페이지에서 각 카테고리를 눌렀을 때 사용되는 API입니다.<br>" +
+                    " 각 카테고리 별로 RequestParam에 넣어서 주시면 해당 값을 반환합니다.")
+    @GetMapping("/revisit")
+    public ApiResponse<ViewMealDiaryDTO.ViewMealDiaryResponseListDTO> getMealDiaryByFoodCategory(
+            @RequestParam Long userId,
+            @RequestParam String foodCategory) {
+
+        ViewMealDiaryDTO.ViewMealDiaryResponseListDTO response = viewMealDiaryService.getMealDiaryByFoodCategory(userId, foodCategory);
 
         return ApiResponse.onSuccess(response);
 

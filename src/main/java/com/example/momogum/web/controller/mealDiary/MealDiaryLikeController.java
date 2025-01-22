95c247968c9905ba5c014e132f2e026a436529f3
gdbs1107 <gdbs1107@naver.com>
2025-01-22 20:14:07 +0900

    [feat] 필요한 API 명세

---
+++ src/main/java/com/example/momogum/web/controller/mealDiary/MealDiaryLikeController.java
@@ -0,0 +1,16 @@
+package com.example.momogum.web.controller.mealDiary;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meal-diaries/likes")
+@Tag(name = "밥일기 관련 API")
+public class MealDiaryLikeController {
+
+
+    // 좋아요 토글형식으로 API 구현
+
+    // 좋아요하는 회원 조회 API
+}

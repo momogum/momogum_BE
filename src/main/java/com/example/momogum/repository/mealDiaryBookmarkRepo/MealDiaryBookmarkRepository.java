450b3c93eabaf2aef76ecba43b35b1b3568a8f0a
gdbs1107 <gdbs1107@naver.com>
2025-01-23 14:00:42 +0900

    [feat] 북마크한 게시글 조회 API 구현

--- src/main/java/com/example/momogum/repository/mealDiaryBookmarkRepo/MealDiaryBookmarkRepository.java
+++ src/main/java/com/example/momogum/repository/mealDiaryBookmarkRepo/MealDiaryBookmarkRepository.java
@@ -3,9 +3,12 @@ package com.example.momogum.repository.mealDiaryBookmarkRepo;
 import com.example.momogum.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MealDiaryBookmarkRepository extends JpaRepository<MealDiaryBookmark,Long> {
 
     Optional<MealDiaryBookmark> findByUserEntityAndMealDiary(UserEntity findUser, MealDiary findMealDiary);
+
+    List<MealDiaryBookmark> findByUserEntity(UserEntity findUser);
 }

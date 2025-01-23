3bfdf7fdc0ddc80de563bbc3382b7591e6545917
gdbs1107 <gdbs1107@naver.com>
2025-01-23 15:34:01 +0900

    [feat] 댓글 작성 API 구현

--- src/main/java/com/example/momogum/service/mealDiaryService/MealDiaryCommentServiceImpl.java
+++ src/main/java/com/example/momogum/service/mealDiaryService/MealDiaryCommentServiceImpl.java
@@ -1,8 +1,15 @@
 package com.example.momogum.service.mealDiaryService;
 
+import com.example.momogum.apiPayLoad.code.status.ErrorStatus;
+import com.example.momogum.apiPayLoad.exception.handler.MealDiaryHandler;
+import com.example.momogum.apiPayLoad.exception.handler.UserEntityHandler;
+import com.example.momogum.domain.MealDiary;
+import com.example.momogum.domain.MealDiaryComments;
+import com.example.momogum.domain.UserEntity;
 import com.example.momogum.repository.mealDiaryCommentsRepo.MealDiaryCommentsRepository;
 import com.example.momogum.repository.mealDiaryRepo.MealDiaryRepository;
 import com.example.momogum.repository.userEntityRepo.UserEntityRepository;
+import com.example.momogum.web.dto.mealDiary.MealDiaryCommentDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +19,42 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class MealDiaryCommentServiceImpl implements MealDiaryCommentService {
 
-    private UserEntityRepository userEntityRepository;
-    private MealDiaryRepository mealDiaryRepository;
-    private MealDiaryCommentsRepository mealDiaryCommentsRepository;
+    private final UserEntityRepository userEntityRepository;
+    private final MealDiaryRepository mealDiaryRepository;
+    private final MealDiaryCommentsRepository mealDiaryCommentsRepository;
+
+
+    @Override
+    public MealDiaryCommentDTO.MealDiaryCommentResponseDTO create(MealDiaryCommentDTO.MealDiaryCommentRequestDTO request){
+
+        MealDiary mealDiary = findMealDiary(request.getMealDiaryId());
+        UserEntity user = findUser(request.getUserId());
+
+        MealDiaryComments newComment = MealDiaryComments.builder()
+                .content(request.getComment())
+                .mealDiary(mealDiary)
+                .user(user)
+                .build();
+
+        MealDiaryComments saveComment = mealDiaryCommentsRepository.save(newComment);
+
+        return MealDiaryCommentDTO.MealDiaryCommentResponseDTO.builder()
+                .mealDiaryCommentId(saveComment.getId())
+                .build();
+    }
+
+
+
+    // 회원 검색 메서드
+    private UserEntity findUser(Long userId) {
+        return userEntityRepository.findById(userId)
+                .orElseThrow(()->new UserEntityHandler(ErrorStatus.MEMBER_NOT_FOUND));
+    }
+
+    // 밥일기 검색 메서드
+    private MealDiary findMealDiary(Long mealDairyId) {
+        return mealDiaryRepository.findById(mealDairyId)
+                .orElseThrow(()->new MealDiaryHandler(ErrorStatus.MEALDIARY_NOT_FOUND));
+    }
 
 }

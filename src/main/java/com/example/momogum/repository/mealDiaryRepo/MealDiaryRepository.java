873531c2e806080eb8407d1a5802ddb36385d25a
Hwigun46 <hy010827@naver.com>
2025-01-23 16:08:57 +0900

    [fix] 쿼리 수정

--- src/main/java/com/example/momogum/repository/mealDiaryRepo/MealDiaryRepository.java
+++ src/main/java/com/example/momogum/repository/mealDiaryRepo/MealDiaryRepository.java
@@ -2,12 +2,10 @@ package com.example.momogum.repository.mealDiaryRepo;
 
 import com.example.momogum.domain.MealDiary;
 import com.example.momogum.domain.UserEntity;
-import com.example.momogum.web.dto.viewMealDiary.ViewMealDiaryDTO;
+import com.example.momogum.domain.common.enums.IsRevisit;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 import java.util.List;
 
@@ -16,9 +14,11 @@ public interface MealDiaryRepository extends JpaRepository<MealDiary,Long> {
     List<MealDiary> findByUserEntity(UserEntity userEntity);
     @Query("SELECT m.id FROM MealDiary m WHERE m.isRevisit = :isRevisit ORDER BY m.likesCount DESC")
     List<Long> findAllByIsRevisit(@Param("isRevisit") String isRevisit);
+    @Query("SELECT m.id FROM MealDiary m WHERE m.isRevisit = :isRevisit AND m.user.Id!= :userId ORDER BY m.likesCount DESC")
+    List<Long> findAllByIsRevisit(@Param("isRevisit") IsRevisit isRevisit, @Param("userId")Long userId);
 
-    @Query("SELECT m.id FROM MealDiary m WHERE m.foodCategory = :foodCategory ORDER BY m.likesCount DESC")
-    List<Long> findAllByFoodCategory(@Param("foodCategory") String foodCategory);
+    @Query("SELECT m.id FROM MealDiary m WHERE m.foodCategory = :foodCategory AND m.user.Id != :userId ORDER BY m.likesCount DESC")
+    List<Long> findAllByFoodCategory(@Param("foodCategory") String foodCategory, @Param("userId")Long userId);
 
 
     List<MealDiary> findByIdIn(List<Long> ids);

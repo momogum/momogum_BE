752d791c4ca505a20be610bed3c57e4a627eb8ae
Hwigun46 <hy010827@naver.com>
2025-01-24 15:56:43 +0900

    [fix] rebase

--- src/main/java/com/example/momogum/repository/mealDiaryRepo/MealDiaryRepository.java
+++ src/main/java/com/example/momogum/repository/mealDiaryRepo/MealDiaryRepository.java
@@ -12,12 +12,11 @@ import java.util.List;
 public interface MealDiaryRepository extends JpaRepository<MealDiary,Long> {
 
     List<MealDiary> findByUserEntity(UserEntity userEntity);
-    @Query("SELECT m.id FROM MealDiary m WHERE m.isRevisit = :isRevisit ORDER BY m.likesCount DESC")
-    List<Long> findAllByIsRevisit(@Param("isRevisit") String isRevisit);
-    @Query("SELECT m.id FROM MealDiary m WHERE m.isRevisit = :isRevisit AND m.user.Id!= :userId ORDER BY m.likesCount DESC")
+
+    @Query("SELECT m.id FROM MealDiary m WHERE m.isRevisit = :isRevisit AND m.userEntity.Id!= :userId ORDER BY m.likesCount DESC")
     List<Long> findAllByIsRevisit(@Param("isRevisit") IsRevisit isRevisit, @Param("userId")Long userId);
 
-    @Query("SELECT m.id FROM MealDiary m WHERE m.foodCategory = :foodCategory AND m.user.Id != :userId ORDER BY m.likesCount DESC")
+    @Query("SELECT m.id FROM MealDiary m WHERE m.foodCategory = :foodCategory AND m.userEntity.Id != :userId ORDER BY m.likesCount DESC")
     List<Long> findAllByFoodCategory(@Param("foodCategory") String foodCategory, @Param("userId")Long userId);
 
 

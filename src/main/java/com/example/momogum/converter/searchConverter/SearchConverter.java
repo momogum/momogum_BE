a649b42058e14d178492ba9dda1bcb35cfcb9b29
Hwigun46 <hy010827@naver.com>
2025-02-02 02:12:47 +0900

    [feat] converter 구현

---
+++ src/main/java/com/example/momogum/converter/searchConverter/SearchConverter.java
@@ -0,0 +1,32 @@
+package com.example.momogum.converter.searchConverter;
+
+import com.example.momogum.domain.*;
+import com.example.momogum.domain.common.enums.IsRevisit;
+import com.example.momogum.web.dto.search.SearchDTO;
+
+import java.util.List;
+
+public class SearchConverter {
+
+    public static SearchDTO.PostSearchResponseDTO toPostSearchResponseDTO(MealDiary mealDiary) {
+
+        return SearchDTO.PostSearchResponseDTO.builder()
+                .mealDiaryId(mealDiary.getId())
+                .foodImageURL(mealDiary.getMealDiaryImages().stream().findFirst().map(MealDiaryImage::getImageLink).orElse(null))
+                .userImageURL(mealDiary.getUserEntity().getProfileImage())
+                .foodName(mealDiary.getMealDiaryKeywords().stream().findFirst().map(MealDiaryKeyword::getKeyword).map(Keyword::getKeyword).orElse("n/a"))
+                .isRevisit(mealDiary.getIsRevisit())
+                .build();
+    }
+
+    public static SearchDTO.AccountSearchResponseDTO toAccountSearchResponseDTO(UserEntity user) {
+
+        return SearchDTO.AccountSearchResponseDTO.builder()
+                .userId(user.getId())
+                .userName(user.getName())
+                .userNickName(user.getNickname())
+                .userImageURL(user.getProfileImage())
+                .build();
+    }
+
+}

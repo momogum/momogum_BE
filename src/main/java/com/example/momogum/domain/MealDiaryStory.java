4a98d1d38995142e7b3d8c71154a4aaf1505aab7
gdbs1107 <gdbs1107@naver.com>
2025-02-01 16:17:10 +0900

    [feat] 밥일기 스토리 저장 기능 구현

--- src/main/java/com/example/momogum/domain/MealDiaryStory.java
+++ src/main/java/com/example/momogum/domain/MealDiaryStory.java
@@ -17,6 +17,13 @@ public class MealDiaryStory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 업로드한 회원의 이름을 필드로 잡았습니다
+     * 매번 조회 로직 실행할때마다 회원을 조회하는게 아니라 회원의 name을 초반에 필드로 저장해두고
+     * 밥일기 엔티티만 조회하도록 구현하겠습니다
+     * */
+    private String name;
+
     @OneToMany(mappedBy = "mealDiaryStory",cascade = CascadeType.ALL)
     private List<MealDiaryImage> mealDiaryImages = new ArrayList<>();
 
--- src/main/java/com/example/momogum/service/mealDiaryService/MealDiaryStoryServiceImpl.java
+++ src/main/java/com/example/momogum/service/mealDiaryService/MealDiaryStoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.momogum.service.mealDiaryService;
 
+import com.example.momogum.repository.mealDiaryRepo.MealDiaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class MealDiaryStoryServiceImpl {
+
+    private final MealDiaryRepository mealDiaryRepository;
 }

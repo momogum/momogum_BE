64ee171b29860d07619fd9c92c16ed0ce0c23555
Hwigun46 <hy010827@naver.com>
2025-02-02 20:32:26 +0900

    [feat] api 생성

---
+++ src/main/java/com/example/momogum/web/controller/search/SearchController.java
@@ -0,0 +1,46 @@
+package com.example.momogum.web.controller.search;
+
+
+import com.example.momogum.apiPayLoad.ApiResponse;
+import com.example.momogum.service.searchService.SearchService;
+import com.example.momogum.web.dto.mealDiary.MealDairiesDTO;
+import com.example.momogum.web.dto.search.SearchDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/search")
+@Tag(name = "검색 API")
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @Operation(summary = "계정 검색 API")
+    @GetMapping("/account")
+    public ApiResponse<List<SearchDTO.AccountSearchResponseDTO>> getAccountSearch(
+            @RequestParam String request) {
+
+        List<SearchDTO.AccountSearchResponseDTO> result = searchService.getAccountSearch(request);
+
+        return ApiResponse.onSuccess(result);
+    }
+
+
+    @Operation(summary = "밥일기 검색 API")
+    @GetMapping("/mealdiary")
+    public ApiResponse<List<SearchDTO.PostSearchResponseDTO>> getPostSearch(
+            @RequestParam String request,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "6") int size) {
+
+        List<SearchDTO.PostSearchResponseDTO> result = searchService.getPostSearch(request,page,size);
+
+        return ApiResponse.onSuccess(result);
+    }
+}

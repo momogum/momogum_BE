8d1f7dd5972abe67859784c19f1f0eb46b544613
Dobby19 <southvi1@naver.com>
2025-02-12 03:39:20 +0900

    feat: 상대 유저 팔로워/팔로잉 목록 조회 #76

--- src/main/java/com/example/momogum/service/userProfileService/TargetProfileService.java
+++ src/main/java/com/example/momogum/service/userProfileService/TargetProfileService.java
@@ -1,5 +1,6 @@
 package com.example.momogum.service.userProfileService;
 
+import com.example.momogum.web.dto.FollowDTO;
 import com.example.momogum.web.dto.user.UserDTO;
 import com.example.momogum.web.dto.viewMealDiary.ViewMealDiaryDTO;
 import com.example.momogum.web.dto.viewMealDiary.ViewMealDiaryDTO.ViewMealDiaryResponse;
@@ -12,4 +13,8 @@ public interface TargetProfileService {
   List<ViewMealDiaryResponse> getTargetMealDiaries(Long targetUserId);
 
   List<ViewMealDiaryDTO.ViewMealDiaryResponse> getTargetBookmarkedMealDiaries(Long targetUserId);
+
+  List<FollowDTO.FollowingResponseDTO> getTargetFollowings(Long currentUserId, Long targetUserId);
+
+  List<FollowDTO.FollowerResponseDTO> getTargetFollowers(Long currentUserId, Long targetUserId);
 }

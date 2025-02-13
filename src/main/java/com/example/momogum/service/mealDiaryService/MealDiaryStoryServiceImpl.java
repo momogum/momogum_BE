cc8ae83b8f4aaf18bee6e92457eaddc92d20da5f
gdbs1107 <gdbs1107@naver.com>
2025-02-13 21:38:49 +0900

    [feat] 스토리 delete 메서드 구현

--- src/main/java/com/example/momogum/service/mealDiaryService/MealDiaryStoryServiceImpl.java
+++ src/main/java/com/example/momogum/service/mealDiaryService/MealDiaryStoryServiceImpl.java
@@ -62,6 +62,8 @@ public class MealDiaryStoryServiceImpl implements MealDiaryStoryService {
 
         // 원활한 테스트를 위해 int를 반환
         int deletedCount = mealDiaryStoryRepository.deleteByCreatedAtBefore(threeDaysAgo);
+        log.info("삭제된 스토리 갯수: {}", deletedCount);
+
     }
 
 

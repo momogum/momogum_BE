c87a35839d3066238a30cdb009d90ec291bec8ed
jaexxin <jmsb02@naver.com>
2025-02-14 16:02:58 +0900

    [feat] AppointmentOrchestrator, Controller, DTO 초기 구현 + naming 변경

--- src/main/java/com/example/momogum/converter/appointmentConverter/AppointmentNameConverter.java
+++ src/main/java/com/example/momogum/converter/appointmentConverter/AppointmentNameConverter.java
@@ -2,22 +2,20 @@ package com.example.momogum.converter.appointmentConverter;
 
 import com.example.momogum.converter.Converter;
 import com.example.momogum.domain.appointment.AppointmentName;
-import com.example.momogum.web.dto.appointment.AppointmentNameDTO;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-import static com.example.momogum.web.dto.appointment.AppointMentDTO.*;
 import static com.example.momogum.web.dto.appointment.AppointmentNameDTO.*;
 
 /**
  * 약속 이름 정하기 부분 converter
  */
 @Component
-public class AppointmentNameConverter implements Converter<AppointmentNameResponseDTO, AppointmentName> {
+public class AppointmentNameConverter implements Converter<AppointmentNameRequestDTO, AppointmentName> {
 
     @Override
-    public AppointmentName convert(AppointmentNameResponseDTO request) {
+    public AppointmentName convert(AppointmentNameRequestDTO request) {
 
         return AppointmentName.builder()
                 .name(request.getName())
--- src/main/java/com/example/momogum/web/controller/appointment/AppointmentNameController.java
+++ src/main/java/com/example/momogum/web/controller/appointment/AppointmentNameController.java
@@ -2,8 +2,8 @@ package com.example.momogum.web.controller.appointment;
 
 import com.example.momogum.apiPayLoad.ApiResponse;
 import com.example.momogum.service.appointmentService.AppointmentNameService;
-import com.example.momogum.web.dto.appointment.AppointMentDTO.CreateAppointmentResponseDTO;
-import com.example.momogum.web.dto.appointment.AppointmentNameDTO.AppointmentNameResponseDTO;
+import com.example.momogum.web.dto.appointment.AppointmentDTO.CreateAppointmentResponseDTO;
+import com.example.momogum.web.dto.appointment.AppointmentNameDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -25,7 +25,7 @@ public class AppointmentNameController {
     @Operation(summary = "모임 이름 정하기 API",
             description = "CreateAppointmentNameDTO을 통해 값을 한 번에 입력받습니다.")
     @PostMapping("/name")
-    public ApiResponse<CreateAppointmentResponseDTO> createAppointmentName(@RequestBody @Valid AppointmentNameResponseDTO request) {
+    public ApiResponse<CreateAppointmentResponseDTO> createAppointmentName(@RequestBody @Valid AppointmentNameDTO.AppointmentNameRequestDTO request) {
         Long appointmentNameId = appointmentNameService.creatAppointmentName(request);
         // 성공 응답 반환
         return ApiResponse.onSuccess(

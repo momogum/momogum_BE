e3089b1b3175e516899a5aafbbf37ca480ead42e
gdbs1107 <gdbs1107@naver.com>
2025-02-14 14:48:58 +0900

    [chore] 음식 카테고리 업데이트

--- src/main/java/com/example/momogum/domain/common/enums/FoodCategory.java
+++ src/main/java/com/example/momogum/domain/common/enums/FoodCategory.java
@@ -9,8 +9,11 @@ public enum FoodCategory {
     KOREAN("한식"),
     CHINESE("중식"),
     JAPANESE("일식"),
+    AMERICAN("양식"),
     ASIAN("아시안 푸드"),
-    FAST_FOOD("패스트 푸드");
+    FAST_FOOD("패스트 푸드"),
+    CAFE("카페"),
+    ETC("기타");
 
     private final String description;
 

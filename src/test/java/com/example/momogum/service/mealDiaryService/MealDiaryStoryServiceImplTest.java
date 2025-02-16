78ee4badb7bde531dcdfa98f2dd10dbdd5c07e84
gdbs1107 <gdbs1107@naver.com>
2025-02-16 16:22:42 +0900

    [feat] get메서드 테스트 코드 작성 완료

--- src/test/java/com/example/momogum/service/mealDiaryService/MealDiaryStoryServiceImplTest.java
+++ src/test/java/com/example/momogum/service/mealDiaryService/MealDiaryStoryServiceImplTest.java
@@ -1,9 +1,9 @@
 package com.example.momogum.service.mealDiaryService;
 
-import com.example.momogum.domain.MealDiary;
-import com.example.momogum.domain.MealDiaryImage;
-import com.example.momogum.domain.MealDiaryStory;
-import com.example.momogum.domain.UserEntity;
+import com.example.momogum.apiPayLoad.exception.handler.MealDiaryHandler;
+import com.example.momogum.apiPayLoad.exception.handler.MealDiaryStoryHandler;
+import com.example.momogum.apiPayLoad.exception.handler.UserEntityHandler;
+import com.example.momogum.domain.*;
 import com.example.momogum.domain.common.enums.FoodCategory;
 import com.example.momogum.domain.common.enums.LoginType;
 import com.example.momogum.repository.followRepo.FollowingRepository;
@@ -24,6 +24,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.momogum.apiPayLoad.code.status.ErrorStatus.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -93,6 +94,9 @@ class MealDiaryStoryServiceImplTest {
                 .name(testMember.getName())
                 .mealDiary(testMealDiary)
                 .build();
+
+        spy(testMealDiaryStory);
+        spy(testMealDiary);
     }
 
 
@@ -100,11 +104,6 @@ class MealDiaryStoryServiceImplTest {
     @DisplayName("get()을 이용하여 스토리를 상세 조회 할 수 있다")
     public void get_success() {
         // given
-        spy(testMealDiaryStory);
-        spy(testMealDiary);
-        spy(testMember);
-        spy(testMealDiaryImage);
-
         when(userEntityRepository.findById(any())).thenReturn(Optional.ofNullable(testMember));
         when(mealDiaryStoryRepository.findById(any())).thenReturn(Optional.ofNullable(testMealDiaryStory));
 
@@ -118,5 +117,63 @@ class MealDiaryStoryServiceImplTest {
         assertThat(response.getProfileImageLink()).isEqualTo(null);
         assertThat(response.getLocation()).isEqualTo(testMealDiary.getLocation());
         assertThat(response.getDescription()).isEqualTo(testMealDiary.getDescription());
+    }
+
+
+    @Test
+    @DisplayName("스토리를 조회하면 스토리 조회 엔티티를 생성한다")
+    public void title(){
+        // given
+        when(userEntityRepository.findById(any())).thenReturn(Optional.ofNullable(testMember));
+        when(mealDiaryStoryRepository.findById(any())).thenReturn(Optional.ofNullable(testMealDiaryStory));
+
+        // when
+        mealDiaryStoryService.get(1L, 1L);
+
+        //then
+        verify(mealDiaryStoryViewRepository, times(1)).save(any());
+    }
+
+
+    @Test
+    @DisplayName("존재하지 않는 회원이 스토리를 조회하면 정해진 예외를 반환한다")
+    public void get_fail_memberNotFound(){
+        //given
+        when(userEntityRepository.findById(any())).thenReturn(Optional.empty());
+
+        //when & then
+        assertThatThrownBy(() -> mealDiaryStoryService.get(1L, 1L))
+                .isInstanceOf(UserEntityHandler.class)
+                .hasFieldOrPropertyWithValue("code", MEMBER_NOT_FOUND);
+    }
+
+
+    @Test
+    @DisplayName("존재하지 않는 밥일기를 조회하면 정해진 예외를 반환한다")
+    public void get_fail_mealDiaryNotFound(){
+        //given
+        when(userEntityRepository.findById(any())).thenReturn(Optional.ofNullable(testMember));
+        when(mealDiaryStoryRepository.findById(any())).thenReturn(Optional.empty());
+
+        //when & then
+        assertThatThrownBy(() -> mealDiaryStoryService.get(1L, 1L))
+                .isInstanceOf(MealDiaryStoryHandler.class)
+                .hasFieldOrPropertyWithValue("code", MEALDIARY_STORY_NOT_FOUND);
+    }
+
+
+    @Test
+    @DisplayName("회원의 프로필 이미지가 저장되어 있지 않으면 null을 반환한다" +
+            "회원가입 시, 기본이미지를 저장하는 로직이 구현되면 이 테스트도 예외를 반환하도록 수정해야함")
+    public void get_fail_memberProfileImage_null(){
+        // given
+        when(userEntityRepository.findById(any())).thenReturn(Optional.ofNullable(testMember));
+        when(mealDiaryStoryRepository.findById(any())).thenReturn(Optional.ofNullable(testMealDiaryStory));
+
+        // when
+        MealDiaryStoryReadDTO.MealDiaryStoryReadResponseDTO response = mealDiaryStoryService.get(1L, 1L);
+
+        // then
+        assertThat(response.getProfileImageLink()).isEqualTo(null);
     }
 }
